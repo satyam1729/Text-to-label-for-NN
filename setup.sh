f531c902f8d64a0080b91d81a6e7511335ef92ba
Satyam Yadav <satyamyadav13579@gmail.com>
2017-05-31 11:24:42 +0530

    add script for downloading and extracting data

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #add configuration files stuff here
+echo "Downloading dataset from http://hts.sp.nitech.ac.jp/archives/2.3/HTS-demo_CMU-ARCTIC-SLT.tar.bz2"
+cd data
+wget http://hts.sp.nitech.ac.jp/archives/2.3/HTS-demo_CMU-ARCTIC-SLT.tar.bz2
+echo "extracting dataset"
+tar -xvf HTS-demo_CMU-ARCTIC-SLT.tar.bz2
+cd ..
 chmod 775 tools/setup_tools.sh
 echo 'setting up tools required'
 ./tools/setup_tools.sh
